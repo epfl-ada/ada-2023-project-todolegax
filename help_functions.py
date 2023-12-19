cd11d49ccc7febf1640781c2eb598db26bc75ec5
AxelBegue <axel.beguelin@epfl.ch>
2023-12-19 15:35:00 +0100

    Restructuration

--- help_functions.py
+++ help_functions.py
@@ -5,13 +5,13 @@ def find_closest_match(input_str, target_list):
     """Find the closest match in a list using Hamming distance."""
     return difflib.get_close_matches(input_str, target_list, n=1, cutoff=0.5)
 
-
-def match_country_name(df,country,state,unique_loc,unique_state):
+# Replace name of location by the closest match from the geo dataframe
+def match_country_name(df,unique_loc,unique_state):
     # Init dropped breweries
     dropped_brew = []
 
-    # Loop through the 'brewery_location' column and replace values if needed
-    for i, location in enumerate(df[country]):
+    # Loop through the 'brewery_country' column and replace values if needed
+    for i, location in enumerate(df['country']):
         # Check if in USA --> (USA, state)
         try:
             location, state = location.split(', ')
@@ -24,22 +24,37 @@ def match_country_name(df,country,state,unique_loc,unique_state):
         if closest_match_country:
             # If closest match is USA --> match state      
             if closest_match_country[0] == 'United States of America':
-                df.at[i, country] = closest_match_country[0]
+                df.at[i, 'country'] = closest_match_country[0]
                 
                 # Find state
                 closest_match_state = find_closest_match(state, unique_state)
                 if closest_match_state:
-                    df.at[i, state] = closest_match_state[0]
+                    df.at[i, 'state'] = closest_match_state[0]
                 else:
                     df = df.drop(index=i)
             else:
                 # If not in USA --> state = location 
-                df.at[i, country] = closest_match_country[0]
-                df.at[i, state] = closest_match_country[0]
+                df.at[i, 'country'] = closest_match_country[0]
+                df.at[i, 'state'] = closest_match_country[0]
         else:
             #print(i)
-            #print(breweries_loc.at[i, 'brewery_location'])
+            #print(breweries_loc.at[i, 'brewery_country'])
             # If no match, drop
             dropped_brew.append(location)
             df = df.drop(index=i)
     return df, dropped_brew
+
+# Function to calculate Haversine distance
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371.0  # Radius of the Earth in kilometers
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
